4340fd0914baa015fa12998344523f941c06f275
saraUsama9910 <sarausama80@gmail.com>
2024-05-05 19:48:13 +0300

    openai

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,7 @@
+import 'dart:js_interop';
+
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:chatgpt/constants/constants.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 
@@ -9,11 +13,26 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final _openAI = OpenAI.instance.build(
+    token: OPENAI_API_KEY,
+    baseOption: HttpSetup(
+      receiveTimeout: const Duration(
+        seconds: 5,
+      ),
+    ),
+    enableLog: true
+  );
+  // final _openAI = OpenAI.instance.build(
+  //   token: OPENAI_API_KEY,
+  //   baseOption: HttpSetup(
+  //     receiveTimeout: const Duration(seconds: 5),
+  //   ),
+  // );
   final ChatUser _currentUser =
       ChatUser(id: '1', firstName: 'Sara', lastName: 'Mostafa');
   final ChatUser _currentBotUser =
       ChatUser(id: '1', firstName: 'Chat', lastName: 'Bot');
-  List<ChatMessage> _messages = <ChatMessage>[];
+  final List<ChatMessage> _messages = <ChatMessage>[];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +47,11 @@ class _ChatPageState extends State<ChatPage> {
       ),
       body: DashChat(
           currentUser: _currentUser,
+          messageOptions: const MessageOptions(
+            currentUserContainerColor: Color.fromARGB(255, 48, 60, 230),
+            containerColor: Color.fromARGB(255, 75, 117, 255),
+            textColor: Colors.white,
+          ),
           onSend: (ChatMessage m) {
             getChatResponse(m);
           },
@@ -35,5 +59,10 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  Future<void> getChatResponse(ChatMessage m) async {}
+  Future<void> getChatResponse(ChatMessage m) async {
+    print(m.text);
+    setState(() {
+      _messages.insert(0, m);
+    });
+  }
 }
