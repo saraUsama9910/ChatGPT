3b632cb755f57065c313d8d0557befb01264479c
saraUsama9910 <sarausama80@gmail.com>
2024-05-05 19:07:35 +0300

    s

--- lib/constants/api_consts.dart
+++
@@ -1,3 +0,0 @@
-// ignore: constant_identifier_names
-const String OPENAI_API_KEY =
-    "sk-proj-EDiZX1DdXjVQJlSKDBFaT3BlbkFJbH5c4usoYBIXGkput3BD";
--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 
-Color scaffoldBackgroundColor = const Color(0xFF343541);
-Color cardColor = const Color(0xFF444654);
+Color scaffoldBackgroundColor = Color.fromARGB(255, 255, 255, 255);
+Color cardColor = Color.fromARGB(255, 151, 162, 236);
+const  OPENAI_API_KEY =
+    "sk-proj-EDiZX1DdXjVQJlSKDBFaT3BlbkFJbH5c4usoYBIXGkput3BD";
